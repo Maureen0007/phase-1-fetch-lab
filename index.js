eb536fb1578d38b0d090b39290418aa1713fbebf
Maureen0007 <maureen.wanjiku@student.moringaschool.com>
2022-06-18 01:30:53 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
+
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  const fetchFromEndPoint = fetch("https://anapioficeandfire.com/api/books") 
+  .then(res => res.json())
+  .then(booksData => booksData.forEach(book => renderBooks(book)) )
+
+  return fetchFromEndPoint
 }
+
+fetchBooks()
 
 function renderBooks(books) {
   const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
-  });
+  const h2 = document.createElement('h2');
+  h2.innerHTML = books.name;
+  main.appendChild(h2);
+
 }
 
 document.addEventListener('DOMContentLoaded', function() {
